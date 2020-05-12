9019da79f52e7aef26258d325d07102f933d280b
loressl <loryssl@hotmail.com>
2020-05-11 21:23:36 -0300

    Mudança na cor do título.

--- lib/login/login_page.dart
+++ lib/login/login_page.dart
@@ -50,7 +50,7 @@ class _LoginPageState extends State<LoginPage> {
             crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisSize: MainAxisSize.max,
             children: <Widget>[
-              Text("Bem-Vindo", style: TextStyle(color: Colors.black,fontSize: 35, fontWeight: FontWeight.bold),),
+              Text("Bem-Vindo", style: TextStyle(color: Colors.white,fontSize: 35, fontWeight: FontWeight.bold),),
               SizedBox(height: 20.0,),
               Card(
                 shape: RoundedRectangleBorder(
